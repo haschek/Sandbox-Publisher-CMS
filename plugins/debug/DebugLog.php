89f197ba0454373fbf3da7342296f5e3541440ce
Michael Haschke <haschek@eye48.com>
2010-12-13 23:21:09 +0100

    [add] DebugLog plugin: handles log messages and listens to relevant events

---
+++ plugins/debug/DebugLog.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * DebugLog Plugin
+ *
+ * Listens to (log message) events, to save and echo log messages
+ *
+ * @author Michael Haschke, http://eye48.com/
+ */
+class DebugLog extends SandboxPlugin
+{
+    protected $log_messages = array();
+
+    protected function init()
+    {
+
+        $this->pm->subscribe('sandbox_add_log_message', $this, 'addMessage');
+        $this->pm->subscribe('sandbox_end_of_template_header', $this, 'printCssStyles');
+        $this->pm->subscribe('sandbox_end_of_template_body', $this, 'output');
+
+    }
+
+    public function addMessage($msg)
+    {
+        $this->log_messages[] = $msg;
+    }
+
+    public function printCssStyles()
+    {
+        echo '
+            <style type="text/css">
+                #SPCSM_DebugLog
+                {
+                    position:fixed;
+                    left:1em;
+                    top:0;
+                    background:#000; color:#fff;
+                    padding:0.5em;
+                    font-family:monospace;
+                    border-width:3px;
+                    border-color:#fff;
+                    border-style:none double double double;
+                }
+                #SPCSM_DebugLog:hover
+                {
+                    overflow:auto;
+                    right:1em;
+                    bottom:1em;
+                }
+
+                #SPCSM_DebugLog pre
+                {
+                    display:none;
+                }
+                #SPCSM_DebugLog:hover pre
+                {
+                    display:block;
+                }
+            </style>'.PHP_EOL;
+    }
+
+    public function output()
+    {
+        if (!defined('IS_PRODUCTION_INSTANCE') && count($this->log_messages)>0)
+        {
+            echo '<div id="SPCSM_DebugLog"><strong>DEBUGINFO</strong>'.PHP_EOL;
+            echo('<pre>'.print_r($this->log_messages, true).'</pre>').PHP_EOL;
+            echo '</div>'.PHP_EOL;
+        }
+    }
+}
+?>
